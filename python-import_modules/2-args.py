cfa3bc7b7516a28989824e82a366e0285a1808e0
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-29 10:11:15 -0700

    v2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
-        print("{:d} argument:".format(len(argv) - 1))
+        print("{} argument:".format(len(argv) - 1))
     else:
-        print("{:d} arguments:".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))

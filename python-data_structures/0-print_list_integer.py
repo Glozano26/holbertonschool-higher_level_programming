e2845418ee2b7ae23849efdf39027400bccfea56
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-01 23:20:23 -0700

    v4

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(i))
-
-if __name__ == '__main__':
-    print_list_integer(my_list)
+    for n in range(0, len(my_list)):
+        print("{:d}".format(int(my_list[n])))

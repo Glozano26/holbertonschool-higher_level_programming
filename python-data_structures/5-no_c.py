a56cc4eee04e9b0671a0a34d714d5b3f6d214c8e
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-03 10:09:20 -0700

    replace v3

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-#del_char = "c"
+    del_char1 = "c"
+    del_char2 = "C"
     new_str = ""
     for char in my_string:
-        if char != "c" and char != "C":
+        if char != del_char1 and char != del_char2:
             new_str += char
     return new_str

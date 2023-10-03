dbd2828436f555a0ccc8f19c7ba5b688ee26c184
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-03 11:53:22 -0700

    replace v6

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,10 +2,8 @@
 def new_in_list(my_list, idx, element):
     list_cop = my_list.copy()
     if idx < 0:
-        list_cop = copy(my_list)
         return list_cop
     elif idx >= len(my_list):
-        list_cop = copy(my_list)
         return list_cop
     else:
         list_cop[idx] = element

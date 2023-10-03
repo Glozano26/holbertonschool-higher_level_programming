a35c21a4831013871852401e3a5cf26697463b40
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-03 16:20:22 -0700

    find max v3

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,6 +2,7 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    my_list.sort()
-    my_list.reverse()
-    return my_list[0]
+    else:
+        my_list.sort()
+        my_list.reverse()
+        return my_list[0]

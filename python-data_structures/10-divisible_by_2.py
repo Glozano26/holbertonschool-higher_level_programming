3bff6965cc69d69001b37b1938283feabbd9da38
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-03 22:04:12 -0700

    Only by 2 v6

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+
+    multiple = []
+
+    for i in my_list:
+        if i % 2 == 0:
+            multiple.append(True)
+        else:
+            multiple.append(False)
+
+    return multiple

c959a0b4398d1c8006cdffee565b305cf6462dd7
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-05 16:06:13 -0700

    Roman to Integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    n_roma = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    i = 0
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+    while i < len(roman_string):
+        vlr = n_roma[roman_string[i]]
+        if i + 1 < len(roman_string) and n_roma[roman_string[i + 1]] > vlr:
+            total = total - vlr
+        else:
+            total += vlr
+        
+        i = i + 1     
+    return total

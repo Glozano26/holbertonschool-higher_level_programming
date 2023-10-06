63589d9cec7daab3c2e1d9b7b0d711c407219018
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-06 15:06:37 -0700

    Integers division with debug

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        print("{:d} / {:d} = {}".format(a, b, result))
+    except ZeroDivisionError:
+        return None
+    finally:
+        print(f"Inside result: {result}")
+        return result

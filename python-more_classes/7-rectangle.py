bfe5c290b5de15c4955e27ce2897b56f6496e6f5
Glozano26 <135636906+Glozano26@users.noreply.github.com>
2023-10-12 19:05:14 -0500

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):  # Changed 'height' to 'value' here
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")

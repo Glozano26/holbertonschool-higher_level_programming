5ffb6d9c664e56f4bfe557c27d314c54c4c5587e
Glozano26 <135636906+Glozano26@users.noreply.github.com>
2023-10-13 23:33:27 -0500

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,21 +6,21 @@ def text_indentation(text):
     """unction that prints a text with 2 new lines after each of
     these characters: ., ? and :
     """
-    symbols = [".", "?", ":"]
-    if is not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
-        result_esp = ""
-    for char in text:
-        if char == symbols:
-            if char == " ":
-                pass
+    esp_ch = ""
+    for a_char in text:
+        if esp_ch == "." or esp_ch == "?" or esp_ch == ":":
+            if a_char == " ":
+                continue
             else:
-                result_esp = ""
-                print(char, end="")
-                pass
-        if char == symbols:
-            print(char)
+                esp_ch = ""
+                print(a_char, end="")
+                continue
+        if a_char == "." or a_char == "?" or a_char == ":":
+            print(a_char)
             print("")
-            result_esp = char
+            esp_ch = a_char
         else:
-            print(char, end="")
+            print(a_char, end="")
+    

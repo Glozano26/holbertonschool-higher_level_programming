4d195ba1816b67082eda2e8d1c222c5ac711a352
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-19 15:31:27 -0700

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,11 +3,13 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """Geometry module"""
 
+
 class Rectangle(BaseGeometry):
     """Definition of the class Rectangle using the BaseGometry"""
     def __init__(self, width, height):
         """width and height must be private. No getter or setter
-        width and height must be positive integers, validated by integer_validator
+        width and height must be positive integers, validated
+        by integer_validator
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)

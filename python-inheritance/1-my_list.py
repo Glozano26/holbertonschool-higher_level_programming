97edd079fbf212a905945e6325159d676d1e2892
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-19 09:25:08 -0700

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """that prints the list, but sorted ascending sort.
+    You can assume that all the elements of the list will be of type int.
+    """
+    def print_sorted(self):
+        print(sorted(self))

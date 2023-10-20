b433a823897bd7be677e43d8111cf8921eaa1480
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-20 14:02:45 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
-    """Write a function that reads a text file (UTF8) and prints it to stdout"""
+    """Write a function that reads a text file
+    (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")

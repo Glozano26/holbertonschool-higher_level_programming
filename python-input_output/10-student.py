2ed1d8b1cf9b5348f235f74894cde616bf996500
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-20 19:03:57 -0700

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """lass Student that defines a student by:
+    Public instance attributes: first_name, last_name, age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    result[key] = value
+
+            return result

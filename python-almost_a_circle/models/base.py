85af9968a351110f22cd4f5ab19e73e477d58529
Glozano26 <lozsuperbia2014@gmail.com>
2023-10-26 08:48:25 -0700

    Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """JSON is one of the standard formats for sharing data representation"""
         if list_dictionaries is None or list_dictionaries == "":
             my_dict = json.loads(list_dictionaries)
             return my_dict

9d12d8ecee01ecd26f7ece8bb280e197d4186a78
Glozano26 <135636906+Glozano26@users.noreply.github.com>
2023-10-27 12:00:24 -0500

    Update test_base.py

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -142,7 +142,7 @@ class TestBasepep8(unittest.TestCase):
         """test for base file and test_base file pep8"""
         style = pep8.StyleGuide(quiet=True)
         base = "models/base.py"
-        test_base = "tests/test_models/test_base.py"
+        test_base = "tests/test_base.py"
         result = style.check_files([base, test_base])
         self.assertNotEqual(result.total_errors, 0)
 

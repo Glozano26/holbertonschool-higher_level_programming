16ca9468b733695bf4a4667de572122a25b058d5
glozano26 <lozsuperbia2014@gmail.com>
2023-11-22 15:50:08 -0500

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,13 @@
 -- Full creation
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-)
-INSERT INTO second_table
-VALUES
-(1, 'Juan', 10),
-(2, 'Álex', 3),
+CREATE TABLE  IF NOT EXISTS second_table (
+id INT,
+name  VARCHAR(256),
+score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
 (3, 'Bob', 14),
-(4, 'Jorge',8);
+(4, 'George', 8);

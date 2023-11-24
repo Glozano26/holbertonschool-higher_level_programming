8050ed3ebda348ad2dca03fb163526ab566da87c
glozano26 <lozsuperbia2014@gmail.com>
2023-11-24 15:27:45 -0500

    root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 #!/bin/bash
 -- Root user
-CREATE USER  IF NOT EXISTS 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

35626456df3537a7dd22bf1b37af06c40db6ef07
glozano26 <lozsuperbia2014@gmail.com>
2023-11-24 18:07:32 -0500

    States table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
 -- States table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Selecciona la base de datos que recién cree
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL,
     name VARCHAR(256) NOT NULL

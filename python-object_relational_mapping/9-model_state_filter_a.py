9fc9312989c4fb2e6fcb75c10320a2f84bf8088e
glozano26 <lozsuperbia2014@gmail.com>
2023-11-29 17:16:26 -0500

    task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" All states via SQLAlchemy"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+def list_states(username, password, hbtn):
+    """funcion para listar los estados"""
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{hbtn}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+if __name__ == '__main__':
+    """Get arguments  from command-line"""
+    username, password, hbtn = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, hbtn)

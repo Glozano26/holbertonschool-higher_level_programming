128ae74c237dee380ddc66bcff8bbd3112d3407f
glozano26 <lozsuperbia2014@gmail.com>
2023-11-29 14:38:07 -0500

    task model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,5 +9,6 @@ Base = declarative_base()
 class State(Base):
     """class States"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
     name = Column(String(128), nullable=False)

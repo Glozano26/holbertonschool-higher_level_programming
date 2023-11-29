5fb39b967abf0a507bcdf7299a766c61dbe4dcd1
glozano26 <lozsuperbia2014@gmail.com>
2023-11-28 20:29:37 -0500

    Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A script to list states from database hbtn_0e_0_usa."""
+import MySQLdb
+import sys
+
+
+def list_states(username, password, hbtn):
+    """aqui se conceta con la base de datos"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=hbtn)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    """Get arguments  from command-line"""
+    username, password, hbtn = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, hbtn)

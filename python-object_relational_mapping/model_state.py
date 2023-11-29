22de8070aae061212067e8d16e888d981112c44b
glozano26 <lozsuperbia2014@gmail.com>
2023-11-29 14:15:55 -0500

    task model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+"""First state model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128))

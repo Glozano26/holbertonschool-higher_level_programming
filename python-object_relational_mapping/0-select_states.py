65626bdb29b7805a8f91c9b83336298b06832762
glozano26 <lozsuperbia2014@gmail.com>
2023-11-28 20:03:24 -0500

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,20 +3,23 @@
 import MySQLdb
 import sys
 
+
 def list_states(username, password, hbtn):
-    #aqui se conceta con la base de datos
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=hbtn)
+    """aqui se conceta con la base de datos"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=hbtn)
     cur = db.cursor()
-    
+
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
-    
+
     for row in rows:
         print(row)
-        
+
     cur.close()
     db.close()
-    
+
+
 if __name__ == '__main__':
     """Get arguments  from command-line"""
     username, password, hbtn = sys.argv[1], sys.argv[2], sys.argv[3]

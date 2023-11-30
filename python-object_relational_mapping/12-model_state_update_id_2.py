205bf821cbcb78c84ff849b9f806b77e4c1e0ad8
glozano26 <lozsuperbia2014@gmail.com>
2023-11-29 20:00:09 -0500

    task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" All states via SQLAlchemy"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def list_states(username, password, hbtn):
+    """funcion para listar los estados"""
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{hbtn}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter_by(id=2).first()
+
+    states.name = "New Mexico"
+    session.commit()
+
+    session.close()
+
+
+if __name__ == '__main__':
+    """Get arguments  from command-line"""
+    username, password, hbtn = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, hbtn)
